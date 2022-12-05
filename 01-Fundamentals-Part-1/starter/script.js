bdc56e5075d72acbd22ad611d5aaeacfc1ec4703
Angela Clarke <angeladoust@hotmail.com>
2022-12-05 15:16:23 +1000

    values and data types

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,58 @@
+//variables
+// console.log("variables lesson")
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("jonas is a value")
+// console.log(23)
+
+// let firstName = "Jonas";
+// console.log(firstName)
+
+// let PI = 3.1415;
+
+// let myFirstJob = "programmer" //descriptive variable names
+// let myCurentJob = "Teacher"
+
+// console.log(myFirstJob)
+
+// Assignment 1
+
+// let country = "Australia"
+// let continent = "Australia"
+// let population = 9.6
+
+// console.log(country)
+// console.log(continent)
+// console.log(population + "million")
+
+// data types
+console.log("Data types lesson")
+// 7 primitave data types.
+// 1. Number, used for decimals and integers
+// 2. String, sequence of characters used for text
+// 3. Boolean, true or false
+// 4. undefined, value take by a variable not yet defined eg let children; without the value
+// 5. Null, also means empty value
+// 6. Symbol, value that is unique and cant be changed
+// 7. Bigint (es2020) larger integers than the number type can hold
+
+// you do not need to define the data type, the types are determined automatically.
+
+let javascriptIsFun = true
+console.log(javascriptIsFun)
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javascriptIsFun = "Yes"
+console.log(typeof javascriptIsFun);
+
+let year;
+
+console.log(year);
+console.log(typeof year);
+
+year = 1991
+console.log(typeof year);
