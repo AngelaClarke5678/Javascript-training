f16224a101eb97350028cf6936fe0143e22be9f3
Angela Clarke <angeladoust@hotmail.com>
2022-12-07 08:21:26 +1000

    data types lesson

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -26,8 +26,8 @@
 // console.log(continent)
 // console.log(population + "million")
 
-// data types
-console.log("Data types lesson")
+// // data types
+// console.log("Data types lesson")
 // 7 primitave data types.
 // 1. Number, used for decimals and integers
 // 2. String, sequence of characters used for text
@@ -39,20 +39,40 @@ console.log("Data types lesson")
 
 // you do not need to define the data type, the types are determined automatically.
 
-let javascriptIsFun = true
-console.log(javascriptIsFun)
-console.log(typeof true);
-console.log(typeof javascriptIsFun);
-console.log(typeof 23);
-console.log(typeof "Jonas");
+// let javascriptIsFun = true
+// console.log(javascriptIsFun)
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
 
-javascriptIsFun = "Yes"
-console.log(typeof javascriptIsFun);
+// javascriptIsFun = "Yes"
+// console.log(typeof javascriptIsFun);
 
-let year;
+// let year;
 
-console.log(year);
-console.log(typeof year);
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991
-console.log(typeof year);
+// year = 1991
+// console.log(typeof year);
+
+// let isIsland = false;
+// let language
+
+// console.log(isIsland)
+// console.log(language)
+
+console.log("let,const,var episode")
+//let keyword is variables that can change later or declare empty variables
+// const not changable best for birth date
+// var legacy use
+const partyBooking = 5;
+
+
+console.log(assignments)
+language = "English";
+const country = 'Australia';
+const continent = 'Australia'
+const isIsland = true
+isIsland = False
